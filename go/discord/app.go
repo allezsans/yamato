3837fe46a7f197753c3e0e5bee25a7031ae0be90
allezsans <allez.sans.question@gmail.com>
2017-11-29 18:48:59 +0900

    Enable pubg player stats by command

--- go/discord/app.go
+++ go/discord/app.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/allezsans/yamato/go/pubg"
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -17,6 +18,9 @@ var (
 	HelloWorld        = "!helloworld"
 	ChannelVoiceJoin  = "!vcjoin"
 	ChannelVoiceLeave = "!vcleave"
+
+	PubgAPIKey = os.Getenv("PUBG_API_KEY")
+	pubgClient *pubg.API
 )
 
 func Start() {
@@ -31,6 +35,13 @@ func Start() {
 	err = discord.Open()
 	if err != nil {
 		fmt.Println(err)
+	}
+
+	// pubg
+	pubgClient, err = pubg.New(PubgAPIKey)
+	if err != nil {
+		fmt.Printf("error")
+		log.Fatal(err)
 	}
 
 	fmt.Println("Listening...")
@@ -55,6 +66,53 @@ func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		msg := strings.Fields(m.Content)
 		sendMessage(s, c, strings.Join(msg[1:], ""))
 
+	case strings.HasPrefix(m.Content, "!pubg"):
+		msg := strings.Fields(m.Content)
+		args := msg[1:]
+		if len(args) != 4 {
+			sendMessage(s, c, "`!pubg [アカウント名] [na/eu/as/oc/sa/sea/krj] [2017-pre1/2017-pre2/2017-pre3/2017-pre4/2017-pre5] [solo/duo/squad/solo-fpp/duo-fpp/squad-fpp]`")
+			return
+		}
+
+		player, err := pubgClient.GetPlayer(args)
+		if err != nil {
+			sendMessage(s, c, "`!pubg [アカウント名] [na/eu/as/oc/sa/sea/krj] [2017-pre1/2017-pre2/2017-pre3/2017-pre4/2017-pre5] [solo/duo/squad/solo-fpp/duo-fpp/squad-fpp]`")
+			return
+		}
+
+		history, err := pubgClient.GetMatchHistory(player)
+		if err != nil {
+			sendMessage(s, c, "`!pubg [アカウント名] [na/eu/as/oc/sa/sea/krj] [2017-pre1/2017-pre2/2017-pre3/2017-pre4/2017-pre5] [solo/duo/squad/solo-fpp/duo-fpp/squad-fpp]`")
+			return
+		}
+		fmt.Println((*history)[0])
+
+		f := func(s pubg.Stats) bool {
+			param := args[1:]
+			return s.Region == param[0] && s.Season == param[1] && s.Mode == param[2]
+		}
+		stats, err := (*player).GetPlayerStatsFilteredBy(f)
+
+		if err != nil {
+			sendMessage(s, c, "`!pubg [アカウント名] [na/eu/as/oc/sa/sea/krj] [2017-pre1/2017-pre2/2017-pre3/2017-pre4/2017-pre5] [solo/duo/squad/solo-fpp/duo-fpp/squad-fpp]`")
+			return
+		}
+
+		overviewEmbed := NewEmbed().
+			SetImage(player.Avatar).
+			SetTitle("全ステータス").
+			AddField("プレイ回数", pubg.SelectLabel(stats, "Rounds Played")).
+			AddField("キルレ", pubg.SelectLabel(stats, "K/D Ratio")).
+			AddField("ドン勝率", pubg.SelectLabel(stats, "Win %")).
+			AddField("ドン勝回数", pubg.SelectLabel(stats, "Wins")).
+			AddField("スキルレート", pubg.SelectLabel(stats, "Rating")).
+			AddField("ヘッドショット率", pubg.SelectLabel(stats, "Headshot Kill Ratio")).
+			AddField("遠距離キル", pubg.SelectLabel(stats, "Longest Kill")).
+			InlineAllFields().
+			SetColor(0x00ff00).MessageEmbed
+
+		sendEmbed(s, c, overviewEmbed)
+
 	case strings.HasPrefix(m.Content, fmt.Sprintf("%s %s", BotName, ChannelVoiceJoin)):
 		vcsession, _ = s.ChannelVoiceJoin(c.GuildID, os.Getenv("VOICE_CHANNEL_ID"), false, false)
 		vcsession.AddHandler(onVoiceReceived)
@@ -74,5 +132,14 @@ func sendMessage(s *discordgo.Session, c *discordgo.Channel, msg string) {
 	log.Println(">>> " + msg)
 	if err != nil {
 		log.Println("Error sending message: ", err)
+	}
+}
+
+func sendEmbed(s *discordgo.Session, c *discordgo.Channel, embed *discordgo.MessageEmbed) {
+	_, err := s.ChannelMessageSendEmbed(c.ID, embed)
+
+	log.Println(">>> " + embed.Title)
+	if err != nil {
+		log.Println("Error sending embed: ", err)
 	}
 }
--- go/pubg/client.go
+++ go/pubg/client.go
@@ -2,9 +2,12 @@ package pubg
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"sort"
 	"time"
 )
 
@@ -14,78 +17,134 @@ type API struct {
 	BaseURL *url.URL
 }
 
+// JSONTime is RFC3339 format time.
+type JSONTime struct {
+	time.Time
+}
+
+// UnmarshalJSON hooks
+func (j *JSONTime) UnmarshalJSON(b []byte) (err error) {
+	s := string(b)
+	s = s[1 : len(s)-1]
+
+	t, err := time.Parse(time.RFC3339, s)
+	if err != nil {
+		t, err = time.Parse("2006-01-02T15:04:05.99", s)
+	}
+
+	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+	j.Time = t.In(jst)
+	return
+}
+
+// Format for output
+func (j *JSONTime) Format() string {
+	return j.Time.Format("2006-01-02 15:04")
+}
+
 // Player holds the information returned from the API.
 type Player struct {
-	PlatformID     int       `json:"platformId"`
-	AccountID      string    `json:"AccountId"`
-	Avatar         string    `json:"Avatar"`
-	SelectedRegion string    `json:"selectedRegion"`
-	DefaultSeason  string    `json:"defaultSeason"`
-	SeasonDisplay  string    `json:"seasonDisplay"`
-	LastUpdated    time.Time `json:"LastUpdated"`
-	LiveTracking   []struct {
-		Match        int         `json:"Match"`
-		MatchDisplay string      `json:"MatchDisplay"`
-		Season       int         `json:"Season"`
-		RegionID     int         `json:"RegionId"`
-		Region       string      `json:"Region"`
-		Date         string      `json:"Date"`
-		Delta        float64     `json:"Delta"`
-		Value        float64     `json:"Value"`
-		Message      interface{} `json:"message"`
-	} `json:"LiveTracking"`
-	PlayerName    string `json:"PlayerName"`
-	PubgTrackerID int    `json:"PubgTrackerId"`
-	Stats         []struct {
-		Region string `json:"Region"`
-		Season string `json:"Season"`
-		Match  string `json:"Match"`
-		Stats  []struct {
-			Partition    interface{} `json:"partition"`
-			Label        string      `json:"label"`
-			SubLabel     interface{} `json:"subLabel"`
-			Field        string      `json:"field"`
-			Category     string      `json:"category"`
-			ValueInt     interface{} `json:"ValueInt"`
-			ValueDec     float64     `json:"ValueDec"`
-			Value        string      `json:"value"`
-			Rank         interface{} `json:"rank"`
-			Percentile   float64     `json:"percentile"`
-			DisplayValue string      `json:"displayValue"`
-		} `json:"Stats"`
-	} `json:"Stats"`
-	MatchHistory []struct {
-		Id                   int     `json:"Id"`
-		Updated              string  `json:"Updated"`
-		UpdatedJS            string  `json:"UpdatedJS"`
-		Season               int     `json:"Season"`
-		SeasonDisplay        string  `json:"SeasonDisplay"`
-		Match                int     `json:"Match"`
-		MatchDisplay         string  `json:"MatchDisplay"`
-		Region               int     `json:"Region"`
-		RegionDisplay        string  `json:"RegionDisplay"`
-		Rounds               int     `json:"Rounds"`
-		Wins                 int     `json:"Wins"`
-		Kills                int     `json:"Kills"`
-		Assists              int     `json:"Assists"`
-		Top10                int     `json:"Top10"`
-		Rating               float64 `json:"Rating"`
-		RatingChange         float64 `json:"RatingChange"`
-		RatingRank           int     `json:"RatingRank"`
-		RatingRankChange     int     `json:"RatingRankChange"`
-		Kd                   float64 `json:"Kd"`
-		Damage               int     `json:"Damage"`
-		TimeSurvived         float64 `json:"TimeSurvived"`
-		WinRating            int     `json:"WinRating"`
-		WinRank              int     `json:"WinRank"`
-		WinRatingChange      int     `json:"WinRatingChange"`
-		WinRatingRankChange  int     `json:"WinRatingRankChange"`
-		KillRating           int     `json:"KillRating"`
-		KillRank             int     `json:"KillRank"`
-		KillRatingChange     int     `json:"KillRatingChange"`
-		KillRatingRankChange int     `json:"KillRatingRankChange"`
-		MoveDistance         float64 `json:"MoveDistance"`
-	} `json:"MatchHistory"`
+	PubgTrackerID int      `json:"pubgTrackerId"`
+	AccountID     string   `json:"accountId"`
+	Platform      int      `json:"platform"`
+	NickName      string   `json:"nickName"`
+	Avatar        string   `json:"avatar"`
+	SteamName     string   `json:"steamName"`
+	SteamID       string   `json:"steamId"`
+	LastUpdated   JSONTime `json:"lastUpdated"`
+	TimePlayed    int      `json:"timePlayed"`
+	Stats         []Stats  `json:"stats"`
+}
+
+type Stats struct {
+	Region string   `json:"region"`
+	Season string   `json:"season"`
+	Mode   string   `json:"mode"`
+	Status []Status `json:"stats"`
+}
+
+type Status struct {
+	Label        string  `json:"label"`
+	Field        string  `json:"field"`
+	Category     string  `json:"category"`
+	ValueDec     float64 `json:"valueDec"`
+	Value        string  `json:"value"`
+	DisplayValue string  `json:"displayValue"`
+}
+
+// GetPlayerStatsFilterBy returns filtered by specific region
+// 0:Region 1:Season 2:Mode
+func (p *Player) GetPlayerStatsFilteredBy(f func(s Stats) bool) (Stats, error) {
+	for _, v := range p.Stats {
+		if f(v) {
+			return v, nil
+		}
+	}
+	return Stats{}, errors.New("Invalid filter")
+}
+
+func SelectLabel(s Stats, label string) string {
+	for _, v := range s.Status {
+		if v.Label == label {
+			return v.DisplayValue
+		}
+	}
+	return ""
+}
+
+// MatchHistory holds the information returned from the API.
+type MatchHistory []struct {
+	ID                   int      `json:"Id"`
+	Updated              JSONTime `json:"Updated"`
+	UpdatedJS            string   `json:"UpdatedJS"`
+	Season               int      `json:"Season"`
+	SeasonDisplay        string   `json:"SeasonDisplay"`
+	Match                int      `json:"Match"`
+	MatchDisplay         string   `json:"MatchDisplay"`
+	Region               int      `json:"Region"`
+	RegionDisplay        string   `json:"RegionDisplay"`
+	Rounds               int      `json:"Rounds"`
+	Wins                 int      `json:"Wins"`
+	Kills                int      `json:"Kills"`
+	Assists              int      `json:"Assists"`
+	Top10                int      `json:"Top10"`
+	Rating               float64  `json:"Rating"`
+	RatingChange         float64  `json:"RatingChange"`
+	RatingRank           int      `json:"RatingRank"`
+	RatingRankChange     int      `json:"RatingRankChange"`
+	Kd                   float64  `json:"Kd"`
+	Damage               int      `json:"Damage"`
+	TimeSurvived         float64  `json:"TimeSurvived"`
+	WinRating            int      `json:"WinRating"`
+	WinRank              int      `json:"WinRank"`
+	WinRatingChange      int      `json:"WinRatingChange"`
+	WinRatingRankChange  int      `json:"WinRatingRankChange"`
+	KillRating           int      `json:"KillRating"`
+	KillRank             int      `json:"KillRank"`
+	KillRatingChange     int      `json:"KillRatingChange"`
+	KillRatingRankChange int      `json:"KillRatingRankChange"`
+	MoveDistance         float64  `json:"MoveDistance"`
+}
+
+func (m MatchHistory) Len() int {
+	return len(m)
+}
+
+func (m MatchHistory) Swap(i, j int) {
+	m[i], m[j] = m[j], m[i]
+}
+
+func (m MatchHistory) Less(i, j int) bool {
+	return m[i].Updated.Time.Unix() > m[j].Updated.Time.Unix()
+}
+
+func GetMatchHistoryFilteredBy(f func(v MatchHistory) bool, m []MatchHistory) (MatchHistory, error) {
+	for _, s := range m {
+		if f(s) {
+			return s, nil
+		}
+	}
+	return MatchHistory{}, errors.New("Invalid filter")
 }
 
 // SteamInfo holds information returned from GetSteamInfo.
@@ -101,7 +160,7 @@ type SteamInfo struct {
 
 // New creates a new API client.
 func New(key string) (*API, error) {
-	base, err := url.Parse("https://pubgtracker.com/api/")
+	base, err := url.Parse("https://api.pubgtracker.com/v2/")
 	if err != nil {
 		return &API{}, err
 	}
@@ -119,6 +178,8 @@ func (a *API) NewRequest(endpoint string) (*http.Request, error) {
 		return &http.Request{}, err
 	}
 	urlStr := a.BaseURL.ResolveReference(end)
+
+	fmt.Printf(urlStr.String() + "\n")
 
 	req, err := http.NewRequest("GET", urlStr.String(), nil)
 	if err != nil {
@@ -154,8 +215,20 @@ func (a *API) Do(req *http.Request, i interface{}) error {
 }
 
 // GetPlayer returns a player's stats.
-func (a *API) GetPlayer(uname string) (*Player, error) {
-	endpoint := "profile/pc/" + uname
+func (a *API) GetPlayer(param []string) (*Player, error) {
+	endpoint := "profile/pc/"
+	for i, value := range param {
+		switch i {
+		case 0:
+			endpoint += value
+		case 1:
+			endpoint += fmt.Sprintf("?region=%s", value)
+		case 2:
+			endpoint += fmt.Sprintf("&season=%s", value)
+		case 3:
+			endpoint += fmt.Sprintf("&mode=%s", value)
+		}
+	}
 	req, err := a.NewRequest(endpoint)
 
 	if err != nil {
@@ -166,6 +239,25 @@ func (a *API) GetPlayer(uname string) (*Player, error) {
 	err = a.Do(req, &player)
 
 	return &player, err
+}
+
+// GetMatchHistory reutnrns specific player match history.
+func (a *API) GetMatchHistory(player *Player) (*MatchHistory, error) {
+	endpoint := "matches/pc/" + player.AccountID
+	fmt.Printf(player.AccountID + "\n")
+
+	req, err := a.NewRequest(endpoint)
+
+	if err != nil {
+		return &MatchHistory{}, err
+	}
+
+	var matchHistory MatchHistory
+	err = a.Do(req, &matchHistory)
+
+	sort.Sort(matchHistory)
+
+	return &matchHistory, err
 }
 
 // GetSteamInfo retrieves a player's steam information.
